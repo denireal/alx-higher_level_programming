fc7372a5c12f6fc160a2b3ac40cfeaf59ceca6fc
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-11-28 12:40:56 -0800

    task updated

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -7,6 +7,12 @@ character string as an argument and prints the uppercase version of
 each lowercase letter in the string.
 """
 
+def _upper(character):
+    if ord(character) >= 97 and ord(character) <= 122:
+        return (ord(character) - 32)
+    else:
+        return ord(character)
+
 def uppercase(str):
     """
     Prints the uppercase version of the input string followed by a new line.
@@ -18,8 +24,5 @@ def uppercase(str):
 
     for char in str:
         # Convert each character to uppercase without using str.upper()
-        uppercase_char = chr(ord(char) - 32) if 'a' <= char <= 'z' else char
-        result_str += uppercase_char
-
-    # Print the final result followed by a new line
-    print(result_str)
+        result_str += "%c" % _upper(character)
+        print("{:s}".format(result_str))
