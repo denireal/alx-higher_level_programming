f0b0ef2f808bd230b7206994376228af2f37f826
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-11-29 05:29:52 -0800

    tasks updated

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 """
 Author: Wari Denyefa
 Date: November 28, 2023
@@ -8,8 +9,12 @@ Description: This script prints the uppercase alphabet in
 reverse order, excluding letters with even ASCII values.
 """
 
-for lowercase_ascii in reversed(range(ord("a"), ord("z") + 1)):
-    if lowercase_ascii % 2 != 0:
-        uppercase_ascii = lowercase_ascii - 32
-        uppercase_character = "{:c}".format(uppercase_ascii)
-		print(uppercase_character, end="")
+
+for ascii_digit in range (122, 96, -1):
+
+
+    if ascii_digit % 2 == 1:
+
+        ascii_digit = ascii_digit - 32
+
+        print("{:c}".format(ascii_digit), end="")
