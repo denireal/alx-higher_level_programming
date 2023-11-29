65af5066d0436e66d6290bb621afc4aa87ab19b6
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-11-29 05:21:38 -0800

    tasks updated

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -6,11 +6,11 @@ def remove_char_at(str, n):
     Removes a character at the specified index from the given string.
 
     Parameters:
-    - str (str): The input string from which a character will be removed.
+    - str (str): The input string from which a character ll be removed.
     - n (int): The index of the character to be removed.
 
     Returns:
-    - str: The modified string after removing the character at the specified index.
+    - str: Modified string after removing character at d specific index.
     """
     if n < 0:
         return (str)
