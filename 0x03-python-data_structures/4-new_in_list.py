b8b4a73af61a8707905a13c6ca9ea1ed8d6447e2
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-12-03 07:13:57 -0800

    task done

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -12,7 +12,7 @@ def new_in_list(my_list, idx, element):
 
     Returns:
     - A new list with the specified element replaced, or the original list
-	if the index is out of bounds.
+    - if the index is out of bounds.
     """
     # Check if the index is out of bounds
     if idx < 0 or idx > (len(my_list) - 1):
