57c26cd0c1594a628002dbf1ccf0de74959e1d64
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-12-03 06:25:12 -0800

    task done

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -9,14 +9,13 @@ in a formatted string.
 """
 
 
-# Importing the add function from add_0.py module
-from add_0 import add
+if __name__ == "__main__"
+    # Importing the add function from add_0.py module
+    from add_0 import add
 
-# Assigning values to variables a and b
-a = 1
-b = 2
+    # Assigning values to variables a and b
+    a = 1
+    b = 2
 
-# Main block to ensure the code is only executed when this script is run, not when imported
-if __name__ == "__main__":
-    # Calling the add function and printing the result in a formatted string
+    # Calling add func and printing result in a formatted string
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
