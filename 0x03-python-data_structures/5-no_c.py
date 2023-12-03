227db7c61f640ef61785fb733897132d093608a1
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-12-03 07:16:22 -0800

    task done

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -12,10 +12,10 @@ def no_c(my_string):
     - A new string with all occurrences of 'c' and 'C' removed.
     """
     # Create a new list containing only characters that are not 'c' or 'C'
-    filtered_chars = [char for char in my_string if char.lower() not in {'c', 'C'}]
+    f_chars = [char for char in my_string if char.lower() not in {'c', 'C'}]
 
     # Join the filtered characters to form a new string
-    result_string = "".join(filtered_chars)
+    result_string = "".join(f_chars)
 
     # Return the modified string
     return (result_string)
