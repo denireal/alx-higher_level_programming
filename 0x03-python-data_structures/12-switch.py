458b7eadf4488a26d400f293198f103cee83d4c1
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-12-03 06:07:51 -0800

    tasks done

--- 0x03-python-data_structures/12-switch.py
+++ 0x03-python-data_structures/12-switch.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
-
-# Initial values
 a = 89
 b = 10
-
-# Swap values using tuple unpacking assignment
 a, b = b, a
-
-# Print the swapped values
 print("a={:d} - b={:d}".format(a, b))
