fb9eb76fafa6fde1f726769f7fe4afe8fa2a49da
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-12-06 10:04:30 -0800

    task 100 done

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,38 +3,20 @@
 
 def weight_average(my_list=[]):
     """
-    Function to calculate the weighted average of a list of tuples.
+    Calculates the weighted average of a list of tuples.
 
     Parameters:
-    - my_list (list): The input list of tuples
+    - my_list: Each tuple contains two values: element and weight.
 
     Returns:
-    - float: Returns 0 if the list is empty or None.
+    - float: Weighted average of the elements.
     """
-    # Check if the list is empty or None
-    if len(my_list) == 0 or my_list is None:
+    if not my_list:
         return 0
-    
-    # Variables to store the sum of weights, sum of weighted products
-    # and the weighted average
-    sum_weights = sum_weighted_products = weighted_average = 0
 
-    # Iterate through each tuple in the list
-    for tuple_item in my_list:
-        # Extract values from the tuple
-        weight, value = tuple_item
+    # Using the sum func to calculate the weighted sum and sum of weights
+    weighted_sum = sum(element * weight for element, weight in my_list)
+    total_weight = sum(weight for _, weight in my_list)
 
-        # Update the sum of weights
-        sum_weights += weight
-
-        # Calculate the weighted product for the current tuple
-        weighted_product = weight * value
-
-        # Update the sum of weighted products
-        sum_weighted_products += weighted_product
-
-    # Calculate the weighted average
-    weighted_average = sum_weighted_products / sum_weights
-
-    # Return the weighted average
-    return weighted_average
+    # Avoid division by zero
+    return float(weighted_sum / total_weight) if total_weight != 0 else 0
