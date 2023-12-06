48c7fb6ba75ba367bfbedd32472d5c624ef952e9
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-12-06 10:11:39 -0800

    task 11 done

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python3
 
+
 def multiply_list_map(my_list=[], number=0):
     """
-    Function to multiply each element in a list by a specified num using map.
+    Multiplies each element of the input list by the given number.
 
     Parameters:
-    - my_list (list): The input list of numbers.
-    - number (int): The number to multiply each element in the list.
+    - my_list (list): List of elements to be multiplied.
+    - number (int): The number to multiply each element by.
 
     Returns:
-    - list: A new list containing result of multiplying each element by the
-    specified number.
+    - list: Resulting list after multiplication.
     """
-    # Use map function to apply the lambda function to each element in the list
-    # The lambda function multiplies each element by the specified number
-    result_list = list(map(lambda x: x * number, my_list))
-
-    # Return the resulting list
-    return result_list
+    return list(map(lambda x: number * x, my_list))
