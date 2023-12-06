56c6a932dfe1189a5c2dd3e6aedd7c703d30c9aa
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-12-06 09:37:50 -0800

    task 101 done

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -3,20 +3,18 @@
 
 def square_matrix_map(matrix=[]):
     """
-    Function to square each element in a matrix using the map function.
+    Takes a 2D matrix as input and returns a new matrix where each element
+    is the square of the corresponding element in the input matrix.
 
     Parameters:
-    - matrix (list of lists): The input matrix.
+    - matrix (list of lists): The input 2D matrix.
 
     Returns:
-    - list of lists: A new matrix with each element squared.
+    - list of lists: The resulting matrix with each element squared.
     """
-    # Use the map function to apply the inner lambda function to each element
-    # in each row
-    # The inner lambda function squares each element in a row
-    # The outer lambda function applies the inner lambda function to each row
-    # in the matrix
-    squared_matx = list(map(lambda x: list(map(lambda y: y * y, x)), matrix))
-
-    # Return the resulting squared matrix
-    return squared_matx
+    # Using the map function to apply a lambda function to each row in the
+    # matrix
+    # The lambda function squares each element in the row
+    # The outer map function applies this process to each row in the matrix
+    # The result is a new matrix with elements squared
+    return list(map(lambda x: list(map(lambda y: y * y, x)), matrix))
