fe333fcaf95983deba0dd7e098abf2747ce39f55
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-12-06 10:19:51 -0800

    task 4 done

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,18 +1,3 @@
 #!/usr/bin/python3
-
-
-def common_elements(set_1, set_2):
-    """
-    Finds the common elements between two sets.
-
-    Args:
-    - set_1 (set): The first input set.
-    - set_2 (set): The second input set.
-
-    Returns:
-    - set: A new set containing the common elements between set_1 and set_2.
-    """
-    # Use the intersection operator to find common elements
-    common_elements_set = set_1 & set_2
-
-    return (common_elements_set)
+def only_diff_elements(set_1, set_2):
+    return (set_1 ^ set_2)
