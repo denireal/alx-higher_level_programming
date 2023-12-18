5ccb6f16f295486e31d5cdd135b4d0625b0da5f3
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-12-18 04:29:26 -0800

    first task

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    printed_count = 0
+    try:
+        for index in range(x):
+            print("{}".format(my_list[index]), end="")
+            printed_count += 1
+    except IndexError:
+        pass
+    print()
+    return (printed_count)
