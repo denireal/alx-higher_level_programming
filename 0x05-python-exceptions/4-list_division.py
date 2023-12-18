37cea69da6d73044e53b999660fa85287f782116
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-12-18 05:18:13 -0800

    fifth task

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,27 +2,18 @@
 
 def list_division(my_list_1, my_list_2, list_length):
     division_results = []
-
-    for i in range(list_length):
+    for i in range(0, list_length):
         try:
-            # Check if indices are within the range of both lists
-            if i >= len(my_list_1) or i >= len(my_list_2):
-                raise IndexError("out of range")
-
-            # Attempt division
             div = my_list_1[i] / my_list_2[i]
-
-            # Append result to division_results
-            division_results.append(div)
-
-        except (TypeError, ValueError):
+        except TypeError:
             print("wrong type")
-            division_results.append(0)
+            div = 0
         except ZeroDivisionError:
             print("division by 0")
-            division_results.append(0)
+            div = 0
         except IndexError:
             print("out of range")
-            division_results.append(0)
-
+            div = 0
+        finally:
+            division_results.append(div)
     return division_results
