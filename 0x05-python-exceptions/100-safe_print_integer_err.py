24698b00a96ce908c2736b5e789fff8ef0cffc25
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-12-18 05:26:58 -0800

    advanced one

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import sys
+
+def safe_print_integer_err(value):
+    try:
+        # Attempt to print the integer representation of 'value'
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
+        # Catch exceptions (ValueError or TypeError) and print to stderr
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return False
