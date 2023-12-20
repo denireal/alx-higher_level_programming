dc9dd95edf1df08325a7c71f3cf25f1104d9fa7e
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-12-20 12:34:45 -0800

    tasks done

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,31 +1,36 @@
 #!/usr/bin/python3
 
 """
-This script defines a Python class named MagicClass.
-MagicClass represents a geometric shape with a radius and includes
-methods for calculating the area and circumference.
+This module defines the MagicClass, which represents a geometric shape with
+a specified radius.
+It provides methods for calculating the area and circumference of the shape.
 """
+
 
 
 import math
 
 class MagicClass:
+    """
+    MagicClass models a geometric shape with a defined radius.
+    It includes methods for computing the area and circumference.
+    """
+
     def __init__(self, radius=0):
         """
-        Constructor method to initialize a MagicClass.
+        Constructor method for initializing a MagicClass instance.
 
         Parameters:
-        - radius (int or float): The radius of the geometric shape
-		- (default is 0).
+        - radius (int or float): Radius of the geometric shape (default is 0)
         """
         self.__radius = 0
-        if type(radius) is not int and type(radius) is not float:
+        if type(radius) not in {int, float}:
             raise TypeError("radius must be a number")
         self.__radius = radius
 
     def area(self):
         """
-        Calculate and return the area of the geometric shape.
+        Compute and return the area of the geometric shape.
 
         Returns:
         float: The area of the geometric shape.
@@ -34,9 +39,14 @@ class MagicClass:
 
     def circumference(self):
         """
-        Calculate and return the circumference of the geometric shape.
+        Compute and return the circumference of the geometric shape.
 
         Returns:
         float: The circumference of the geometric shape.
         """
         return 2 * math.pi * self.__radius
+
+# Example usage:
+# my_magic = MagicClass(radius=5)
+# print(my_magic.area())
+# print(my_magic.circumference())
