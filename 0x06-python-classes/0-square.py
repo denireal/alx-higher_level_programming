56c9bb40660752ff51aafba80bf64e385891a7ee
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-12-20 13:14:56 -0800

    tasks done

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
+"""Square Class.
 
-"""
 This script defines a Python class named Square.
 
 Usage Example:
 
-    Square = __import__('0-square').Square
+    from 0-square import Square
+
+    # Create an instance of the Square class
+    my_square = Square()
+
+    # Print the instance
+    print(my_square)
 """
+
 class Square:
+    """Defines the blueprint of a square."""
     pass
