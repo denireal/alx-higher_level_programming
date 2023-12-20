1c9b237743578e7555649aac10c0f3bcb08e5fc0
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-12-20 13:53:38 -0800

    tasks done

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,27 +1,28 @@
 #!/usr/bin/python3
 
+
 """Square Class.
 
-It contains a class that defines a square.
+This module contains a class that defines a square.
 
 Usage Example:
 
     Square = __import__('5-square').Square
 
-    my_square = Square(3)
+    my_square = Square(5)
     print(type(my_square))
     print(my_square.__dict__)
 """
 
 class Square:
-    """Defines the blueprint of a square.
+    '''Defines the blueprint of a square.
 
     Attributes:
         size (int): An integer indicating the size of the square object.
-    """
+    '''
 
     def __init__(self, size=0):
-        """Object constructor method.
+        '''Object constructor method.
 
         Initializes Square with a given size.
 
@@ -32,17 +33,17 @@ class Square:
         Raises:
             TypeError: If size is not an integer.
             ValueError: If size is less than 0.
-        """
+        '''
         self.size = size
 
     @property
     def size(self):
-        """Retrieves the size of the square."""
+        '''Retrieves the size of the square.'''
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Sets the size of the square.
+        '''Sets the size of the square.
 
         Args:
             value (int): The size to set.
@@ -50,7 +51,7 @@ class Square:
         Raises:
             TypeError: If size is not an integer.
             ValueError: If size is less than 0.
-        """
+        '''
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
@@ -58,12 +59,13 @@ class Square:
         self.__size = value
 
     def area(self):
-        """Calculate and return the area of the square."""
+        '''Calculates and returns the area of the square.'''
         return self.__size ** 2
 
     def my_print(self):
-        """Prints the square using '#' character."""
-        for row in range(self.size):
-            for col in range(self.size):
-                print("#", end="\n" if col == self.size - 1 and row != col else "")
-        print()
+        '''Prints the square using '#' character.'''
+        for _ in range(0, self.__size):
+            [print("#", end="") for _ in range(self.__size)]
+            print("")
+        if self.__size == 0:
+            print("")
