4f03fd0097e6002abef1e38d3dbe2dc31057daf3
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-12-20 13:25:16 -0800

    tasks done

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,18 +1,34 @@
 #!/usr/bin/python3
 
-"""
-This script defines a Python class named Square with a constructor
-to initialize the size of a square.
+"""Square Module.
 
-"""
+This module defines a Python class named Square.
 
+Usage Example:
+
+    from 1-square import Square
+
+    # Create an instance of the Square class with size 3
+    my_square = Square(3)
+
+    # Print the type and dictionary representation of the instance
+    print(type(my_square))
+    print(my_square.__dict__)
+"""
 
 class Square:
-    def __init__(self, size):
-        """
-        Constructor method to initialize the size of the square.
+    """Defines the blueprint of a square.
 
-        Parameters:
-        - size: The size of the square.
+    Attribute:
+        size (int): An integer indicating the size of the square object.
+    """
+
+    def __init__(self, size):
+        """An object constructor method.
+
+        Initializes Square with a given size.
+
+        Args:
+            size (int): An integer representing the object size.
         """
         self.__size = size
