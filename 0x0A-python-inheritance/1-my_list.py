414f4929eec7e99660599cb1444f9ab7608da9fa
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2024-01-09 03:56:34 -0800

    adding 1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+
+
+class MyList(list):
+    """
+    Custom list class that inherits from the built-in list class.
+
+    Attributes:
+        Inherits all attributes and methods from the list class.
+
+    Methods:
+        print_sorted(self): Print the elements of the list in sorted order.
+    """
+
+    def print_sorted(self):
+        """
+        Print the elements of the list in sorted order.
+
+        Args:
+            self: The MyList object.
+
+        Returns:
+            None
+        """
+        new_list = sorted(self)
+        print("{}".format(new_list))
