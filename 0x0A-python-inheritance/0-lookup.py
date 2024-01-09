cce911cf20793cc8656059dd3d0eba6e90c52890
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2024-01-09 03:41:51 -0800

    adding 0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
 
 
+
 def lookup(obj):
     """
     Get a list of attributes and methods of an object.
 
     Args:
-        obj: Any Python object.
+        - obj: Any Python object.
 
     Returns:
-        list: A list of strings representing the attributes and methods of the object.
+        - list: A list of strings representing the attributes and methods of the object.
     """
-
-
-
-return dir(obj)
+    return dir(obj)
