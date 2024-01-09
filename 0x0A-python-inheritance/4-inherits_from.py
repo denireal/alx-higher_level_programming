43097870a4b5d7210c5dddc75f1779df76ec5305
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2024-01-09 09:29:50 -0800

    adding 4-inherits_from.py

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Check if the given object is a subclass instance of the specified class.
+"""
+
+def inherits_from(obj, a_class):
+    """
+    Args:
+        obj: Any Python object.
+        a_class: The class to check against.
+
+    Returns:
+        bool: True if the object is a subclass instance, False otherwise.
+    """
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
