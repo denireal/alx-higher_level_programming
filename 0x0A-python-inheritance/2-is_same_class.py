08ede4151d8734c7109686cab123b2d6042e00e3
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2024-01-09 09:02:56 -0800

    adding 2-is_same_class.py

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def is_same_class(obj, a_class):
+    """
+    Check if the given object is an instance of the specified class.
+
+    Args:
+        obj: Any Python object.
+        a_class: The class to check against.
+
+    Returns:
+        bool: True if the object is an instance of the class, False otherwise.
+    """
+    return isinstance(obj, a_class)
