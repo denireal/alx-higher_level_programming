f7810b1d020bbfdd99c91ad0cfbdb61f32152389
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2024-01-09 09:09:13 -0800

    adding 2-is_same_class.py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -11,4 +11,7 @@ def is_same_class(obj, a_class):
     Returns:
         bool: True if the object is an instance of the class, False otherwise.
     """
-    return isinstance(obj, a_class)
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
