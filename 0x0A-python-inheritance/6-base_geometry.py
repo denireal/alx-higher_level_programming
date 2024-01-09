7496c8f12a5c3e83a2c8e2878bd2273ea1570a9d
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2024-01-09 09:45:06 -0800

    adding 6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Base class for geometry-related operations.
+"""
+
+
+class BaseGeometry:
+
+    def area(self):
+        """
+        Abstract method to be implemented by subclasses.
+
+        Raises:
+            Exception: This method should be implemented in subclasses.
+        """
+        raise Exception("area() is not implemented")
