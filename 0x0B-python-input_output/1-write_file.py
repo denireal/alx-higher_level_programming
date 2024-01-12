ea81eee05e887e800d9576c59f2739534bcc5c9d
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2024-01-12 11:41:31 -0800

    updating task 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,6 +4,7 @@
 Write the given text to a file.
 """
 
+
 def write_file(filename="", text=""):
     """
     Write the given text to a file.
@@ -11,8 +12,6 @@ def write_file(filename="", text=""):
     Args:
         filename: The name of the file to be written.
         text: The text to be written to the file.
-    
-    return: The number of characters written to the file.
     """
     # Open the file in write mode ('w') with utf-8 encoding
     with open(filename, 'w', encoding="utf-8") as f:
