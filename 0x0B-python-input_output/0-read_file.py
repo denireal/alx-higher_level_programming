1ff6932f960469329b140916573f18f1fea85a49
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2024-01-12 01:21:28 -0800

    updating 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,16 +2,14 @@
 
 # Author: Denyefa Wari
 
+
 def read_file(filename=""):
     """
     Read the content of a file and print it.
 
-    :param filename: The name of the file to be read.
+    filename: The name of the file to be read.
     """
     # Open the file in read mode ('r') with utf-8 encoding
-    with open(filename, 'r', encoding="utf-8") as f:
-        # Read the entire content of the file
-        read_data = f.read()
-
+    with open(filename, encoding="utf-8") as f:
         # Print the content without adding an extra newline at the end
-        print(read_data, end='')
+        print(f.read() end='')
