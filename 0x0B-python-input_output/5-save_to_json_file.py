9c2be3f6aa003030d045272b10463654001ebf9c
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2024-01-12 11:53:43 -0800

    updating 5-save_to_json_file.py

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-
+"""Save a Python object to a JSON-formatted file."""
 import json
+
 
 def save_to_json_file(*args):
     """
@@ -8,7 +9,7 @@ def save_to_json_file(*args):
 
     Args:
         args: Variable-length arguments where args[0] is the Python object to be saved
-              and args[1] is the name of the file to save the JSON data to.
+        and args[1] is the name of the file to save the JSON data to.
     """
     with open(args[1], 'w', encoding="utf-8") as f:
         json.dump(args[0], f)
