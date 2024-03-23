9c19c362c73f4693c9a44be04204c98dab4b1bca
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2024-03-23 16:31:06 -0700

    saving task

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -2,28 +2,26 @@
 """
 Displays the state from the database.
 """
-from sys import argv
+import sys
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_state import State
+from model_state import State, Base
+from sqlalchemy.orm import Session
 
 
-def print_first_state(username, password, database_name):
-    engine = create_engine(
-            'mysql+mysqldb://{}:{}@localhost/{}'.format(
-                    username, password, database_name))
+def print_state():
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+
+    rows = session.query(State).all()
+
+    for rows_ in rows:
+        print("{}: {}".format(rows_.__dict__['id'], rows_.__dict__['name']))
 
 
-Session = sessionmaker(bind=engine)
-session = Session()
-first_state = session.query(State).order_by(State.id).first()
-if first_state:
-    print('{}: {}'.format(first_state.id, first_state.name))
-else:
-    print('Nothing')
     session.close()
-    if __name__ == '__main__':
-        if len(argv) == 4:
-            print_first_state(argv[1], argv[2], argv[3])
-            else:
-                print(": {} <username> <password> <database>".format(argv[0]))
+
+if __name__ == "__main__":
+    print_state()
