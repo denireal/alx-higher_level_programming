901dc71a1104a7780a9ba6dc1acedd00b03e39d7
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2024-03-23 13:21:21 -0700

    updating task

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -2,20 +2,26 @@
 """
 cript that lists all cities from the hbtn_0e_4_usa database
 """
+
+
 if __name__ == "__main__":
 
-        import MySQLdb
-            from sys import argv
 
-                conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                                             passwd=argv[2], db=argv[3], charset="utf8")
-                                                 curs = conn.cursor()
-                                                     curs.execute("""SELECT cities.id, cities.name, states.name
-                                                         FROM cities
-                                                             LEFT JOIN states ON cities.state_id = states.id
-                                                                 ORDER BY cities.id ASC""")
-                                                                     rows_ = cursor.fetchall()
-                                                                         for row in rows_:
-                                                                                 print(row)
-                                                                                     curs.close()
-                                                                                         conn.close()
+
+    import MySQLdb
+    from sys import argv
+
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+            passwd=argv[2], db=argv[3], charset="utf8")
+
+    curs = conn.cursor()
+    curs.execute("""SELECT cities.id, cities.name, states.name
+    FROM cities
+    LEFT JOIN states ON cities.state_id = states.id
+    ORDER BY cities.id ASC""")
+    
+    rows_ = cursor.fetchall()
+    for row in rows_:
+        print(row)
+        curs.close()
+        conn.close()
