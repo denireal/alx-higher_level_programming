46e5993d87b9d9d44a1ab28591745f99387e4fc6
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2024-03-23 14:41:15 -0700

    updating task

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -6,18 +6,21 @@ cript that lists all cities from the hbtn_0e_4_usa database
 
 if __name__ == "__main__":
 
-
-
     import MySQLdb
     from sys import argv
 
-    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-            passwd=argv[2], db=argv[3], charset="utf8")
+    conn = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3],
+            charset="utf8")
 
     curs = conn.cursor()
     curs.execute("SELECT cities.id, cities.name, states.name \
             FROM cities JOIN states ON cities.state_id = states.id;")
-    
-    rows_ = cursor.fetchall()
+
+    rows_ = curs.fetchall()
     for row in rows_:
         print(row)
