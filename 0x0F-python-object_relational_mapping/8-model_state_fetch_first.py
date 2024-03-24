963ff35f77e63229b3a62e8b7c3d700baac36dcf
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2024-03-24 13:43:43 -0700

    saving task

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,27 +1,21 @@
 #!/usr/bin/python3
 """
-Displays the state from the database.
+Displays the first State object from the hbtn_0e_6_usa DB
 """
 import sys
-from sqlalchemy import create_engine
-from model_state import State, Base
-from sqlalchemy.orm import Session
-
-
-def print_state():
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}?autocommit=true".format(
-        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-
-    session = Session(engine)
-    first_state = session.query(State).order_by(State.id).first()
-
-if first_state:
-    print("{}: {}".format(first_state.id, first_state.name))
-else:
-    print("Nothing")
-    session.close()
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-    print_state()
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    i = session.query(State).first()
+    if i is None:
+        print("Nothing")
+    else:
+        print(i.id, i.name, sep=": ")
