6a36247e62f76201d5e8acb708b0109245fbe9c8
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2024-03-31 14:56:03 -0700

    adding tasks

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 # This script retrieves the Content-Length header from a URL using curl
 
-curl -sI "$1" | grep -i Content-Length | cut -d " " -f 2
+# curl -sI "$1" | grep -i Content-Length | cut -d " " -f 2
+# Check if the number of arguments provided is not equal to 1
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+response_body=$(curl -s "$1")
+size=$(echo -n "$response_body" | wc -c)
+echo "Size of the response body: $size bytes"
