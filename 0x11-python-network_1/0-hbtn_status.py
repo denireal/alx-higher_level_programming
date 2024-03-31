e74824d0a765f56f6a1e42b8ee65a8729e60c525
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2024-03-31 13:21:12 -0700

    updating task

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,7 +10,9 @@ from urllib import request
 if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
     with request.urlopen(url) as response:
-        content = response.read().decode('utf-8')
+        content = response.read()
+        utf8_content = content.decode('utf-8')
         print("Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(utf8_content))
