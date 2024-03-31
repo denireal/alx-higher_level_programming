e4ca13de1abdc7fc7a1dc3a68f6530749e5a4199
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2024-03-31 15:04:03 -0700

    adding tasks

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -3,5 +3,5 @@
 # Send a GET request to the URL specified as the first argument
 # The -L option instructs curl to follow redirects
 # The -s option suppresses progress meter and other unnecessary information
-# The -S option displays errors if they occur
-curl -LsS "$1"
+# The -S option displays errors
+curl -sfL "$1" -X GET
