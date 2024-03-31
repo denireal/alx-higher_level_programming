b0865bb2ba7967a36bc4dd99fa10a8ce6fc94298
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2024-03-31 12:25:05 -0700

    saving task

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# Bash script to retrieve the content of a URL and count the number of characters.
+# Bash script to retrieve the content of a URL and count the number of responses
 
-curl -s "${1}" | wc -c
+curl -sI "$1" | grep Content-Length | cut -d " " -f2
