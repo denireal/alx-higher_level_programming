01a1c4b0e9360b859dc336f0a3b3992004781ee3
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2024-03-31 13:14:25 -0700

    updating task

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,15 +5,12 @@ This script sends an HTTP GET request to 'https://intranet.hbtn.io/status'
 and prints information about the response content.
 """
 
-import urllib.request
+from urllib import request
 
 if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
-    req = urllib.request.Request(url)
-    
-    with urllib.request.urlopen(req) as response:
+    with request.urlopen(url) as response:
         content = response.read().decode('utf-8')
-        
         print("Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
