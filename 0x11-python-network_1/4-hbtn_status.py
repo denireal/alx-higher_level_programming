62327fb91a5c5b545453017b7e01cd82b1193567
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2024-03-31 14:43:28 -0700

    adding tasks

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -9,13 +9,13 @@ import requests
 if __name__ == "__main__":
     # URL to send the GET request
     url = "https://alx-intranet.hbtn.io/status"
-    
+
     # Send a GET request to the URL
     response = requests.get(url)
-    
+
     # Get the content of the response
     content = response.text
-    
+
     # Print information about the response content
     print('Body response:')
     print('\t- type: {}'.format(type(content)))
