ec90755f543c940c292c73f2559ef9ca82205644
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2024-03-31 12:54:44 -0700

    adding task

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,9 +8,12 @@ and prints information about the response content.
 import urllib.request
 
 if __name__ == "__main__":
-    url = 'https://intranet.hbtn.io/status'
-    with urllib.request.urlopen(url) as response:
+    url = 'https://alx-intranet.hbtn.io/status'
+    req = urllib.request.Request(url)
+    
+    with urllib.request.urlopen(req) as response:
         content = response.read().decode('utf-8')
+        
         print("Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
