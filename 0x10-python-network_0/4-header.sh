7edd417423c4b3dd1f55b113b53518fc86fe3027
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2024-04-02 12:11:11 -0700

    saving task

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Takes a URL as an argument, sends a GET request to the URL,
 # and finally prints the body of the response
-curl -s "$1" -H "X-School-User-Id: 98"
+
+curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
