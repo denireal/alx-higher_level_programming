8243d77d7c37cdbd83102e601e1c0466839abdc0
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2024-04-02 09:00:51 -0700

    updating task

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # Script to send a DELETE request to a URL using curl
 
+# Output a message indicating a DELETE request is being made
+echo "I'm a DELETE request"
+
 # Send a DELETE request to the provided URL and suppress output
 curl -s "$1" -X DELETE
